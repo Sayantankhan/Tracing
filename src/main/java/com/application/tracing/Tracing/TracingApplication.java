0d428bc3f887c161168cbb3efdb93606110feaf8
Sayantankhan <iamstk14@gmail.com>
2018-05-05 14:22:50 +0530

    initial project commit

---
+++ src/main/java/com/application/tracing/Tracing/TracingApplication.java
@@ -0,0 +1,12 @@
+package com.application.tracing.Tracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracingApplication.class, args);
+	}
+}
